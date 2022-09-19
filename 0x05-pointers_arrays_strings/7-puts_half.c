26a40cf14aa0d5437bef2a837c957d5e59e60932
Robel-rai <robelabera02@gmail.com>
2022-09-19 03:24:29 -0700

    E7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,15 @@
 
 void puts_half(char *str)
 {
-	int n;
-	n = (sizeof(str)/2);
+	int len = 0;
+	
+	for (len = 0; str[len] != '\0'; len++)
+		;
 
-	for (int i; i < n; i++)
+	for (int i = (len + 1) / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
+
