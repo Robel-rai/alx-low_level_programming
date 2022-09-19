b0a7bef4d73506b5572d1392bee0c17699a8a177
Robel-rai <robelabera02@gmail.com>
2022-09-19 12:31:25 -0700

    E8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * print_array - responsible for printing out the materials
+ * @i: is the program itration determiner
+ * @
+ * Description: the program is going to print out the stored array deppending on the give int n
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0;  i <= n; i++)
+	{
+		printf("%d, " a[i]);
+	}
+	putchar('\n');
+}
