fa0b9cdce89bba3746a3c04ac6f0574b1368978a
Robel-rai <robelabera02@gmail.com>
2022-09-19 03:08:05 -0700

    e6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
+ * put2 - jumpes every odd number and prints the odd one
+ * @i: input string.
  * Return: the pointer to dest.
  */
 
 void puts2(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-		{	
+		{
 			_putchar(str[i]);
 		}
 		i++;
