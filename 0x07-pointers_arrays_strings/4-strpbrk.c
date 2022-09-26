05f7bd76ae831eaf3f2227dcc89be9a8ff86b338
Robel-rai <robelabera02@gmail.com>
2022-09-26 12:22:00 -0700

    question 4 - 8

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/*
+ * _strspn - a function that searches a string for any of 
+ * a set of bytes.
+ *
+ * @s: the first string
+ * @accept: subtracting parameter
+ *
+ * Return - the initial chars of s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+				return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
