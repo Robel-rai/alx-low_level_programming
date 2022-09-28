58be0ecc83bf27fe9d22c56b4ef8cdc1fad7db9d
Robel-rai <robelabera02@gmail.com>
2022-09-28 11:54:42 -0700

    edit to 0 1 and 2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _puts_recursion -  a function that prints a string,
  * followed by a new line.
  * @s: string to be printed
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-/*
+/**
+ * _print_rev_recursion - a function that prints a string in reverse.
  *
+ *@s: The main value 
  */
 
 void _print_rev_recursion(char *s)
