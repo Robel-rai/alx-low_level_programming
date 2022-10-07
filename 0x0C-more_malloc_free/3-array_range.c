44734c28008566a9eb7221b7c5f3edd79d74beac
Robel-rai <robelabera02@gmail.com>
2022-10-07 04:33:01 -0700

    Question 3 Edit yet again

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *array_range -  create an array of integers
@@ -9,41 +10,21 @@
 
 int *array_range(int min, int max)
 {
-int *m;
-int i;
+	int *m;
+	int i;
 
-if (min > max)
-	return (NULL);
+	if (min > max)
+		return (NULL);
 
-m = malloc((max - min + 1) * sizeof(int));
+	m = malloc((max - min + 1) * sizeof(int));
 
-if (m == 0)
-	return (NULL);
+	if (m == 0)
+		return (NULL);
 
 
-for (i = 0; min + i <= max; i++)
-	m[i] = min + i;
+	for (i = 0; min + i <= max; i++)
+		m[i] = min + i;
 
-return (m);
+	return (m);
 }
 
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
