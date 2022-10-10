2a1ed1f4fbe0ff279379a30e8031caf21196ea70
Robel-rai <robelabera02@gmail.com>
2022-10-10 08:20:21 -0700

    Q1' gut '

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+/**
+ * struct dog - initializes the main function
+ * @name: String type
+ * @age: float type
+ * @owner: string type
+ *
+ * Description - The function to intialize the function
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d
+	{
+		char *n;
+		float a;
+		char *o;
+	};
+}
