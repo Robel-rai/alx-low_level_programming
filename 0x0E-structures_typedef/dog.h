dd380c85f7dfeea2cdd6ff4b3e71abe4cf8e701c
Robel-rai <robelabera02@gmail.com>
2022-10-10 05:43:09 -0700

    Added Readme file and Question 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - houses an information about a dog
+ * @name: name of the dog
+ * @age: dog age
+ * @owner: owners name
+ *
+ * Description: for a later use I have defind a dog structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
