db717b24bdc716f131ae3ae553b26db248022a27
Robel-rai <robelabera02@gmail.com>
2022-10-10 22:37:33 -0700

    Question 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * Description: for a later use I have defind a dog structure
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
