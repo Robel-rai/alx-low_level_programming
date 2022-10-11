48c4889ede928eb1de703f516b50056a412ce36a
Robel-rai <robelabera02@gmail.com>
2022-10-11 01:56:21 -0700

    Corration to all the problems

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - print the name of the file that was compiled
- * Return: int
+ * Return: 0
  */
 
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
