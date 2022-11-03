71dd39a1b13c286d91f98c04743e91d4d05a05eb
Robel-rai <robelabera02@gmail.com>
2022-11-03 10:51:24 -0700

    Hello q0 and 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,39 @@
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile - a function that reads a
+ * text file and prints it to the POSIX standard output.
  *
- * @char: is the character file name
- * @letter: is a struct
+ * @filename: is the character file name
+ * @letters: nuber of the letters,
  *
  * Return: 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t read, write;
+	char *buf;
 
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	read = read(fd, buf, letters);
+	write = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (read);
 }
