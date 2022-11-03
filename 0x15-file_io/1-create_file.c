a0ef44a924c70ae8c3bce5d7527af3f35bccefaa
Robel-rai <robelabera02@gmail.com>
2022-11-03 11:07:56 -0700

    Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[strleng]; strleng++)
+	for (strleng = 0; text_content[strleng]; strleng++)
 		;
 
 	rewrite = write(fd, text_content, strleng);
