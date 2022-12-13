e7e50a5a68036c87f593bc3d240c011834f7b78f
Robel-rai <robelabera02@gmail.com>
2022-12-12 23:52:25 -0800

    advanced task 2

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - takes in two numbers addes them then returns
+ * @x: number being added
+ * @y: number being added
+ * Return: 0
+ */
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
